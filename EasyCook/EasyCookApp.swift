53159aadeaf910d85d7642f2c038befaf134f7d5
daniel jermaine <danieljermaine2@gmail.com>
2023-05-15 22:05:02 +0100

    Initial Commit

---
+++ EasyCook/EasyCookApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasyCookApp.swift
+//  EasyCook
+//
+//  Created by Apple on 15/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct EasyCookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
